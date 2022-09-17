c10cbb6fea60001c4b469aa3631a003671558e8a
SihomaSa <sandy.ochoa.27@unsch.edu.pe>
2022-09-17 00:48:05 -0500

    cuarta tarea

---
+++ Tarea_01/codigo.js
@@ -0,0 +1,16 @@
+let numero = parseInt(prompt("Ingrese un numero por favor: "));
+
+if (numero % 5){
+    console.log("Si, el número "+numero+ " es múltiplo de 5");
+}else{
+    console.log("No, el número "+numero+ " no es múltiplo de 5");
+}
+if(( numero % 7 ) == 0){
+    
+    alert(numero + ' es multiplo de 7');
+        
+}else{
+    
+    alert(numero + ' no es multiplo de 7');
+    
+}
